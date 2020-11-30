00cacbf2c33c9f2f4b7d7750bce5e1437fe121d1
Guglielmo Pellitteri <67461869+lateral-gug@users.noreply.github.com>
2020-11-30 17:34:16 +0100

    click on (x,y) to plot |E(x,y| and V(x,y)

--- electric_field.py
+++ electric_field.py
@@ -6,11 +6,11 @@ plt.rcParams['image.cmap'] = 'jet'
 plt.rcParams['axes.facecolor'] = 'k'
 
 #charges values and positions
-qs =   [[-1,0.5,0],
-        [1,-0.5,0]]
+qs =   [[1,0.5,0],
+        [-1,-0.5,0]]
 
 #n^2 points will be plotted
-n = 15
+n = 20
 
 xlim, ylim = 1,1
 a = np.linspace(-xlim,xlim,n)
@@ -22,6 +22,11 @@ def e(q,x,y):
     den = (np.hypot(x-q[1],y-q[2]))**3
     return q[0]*(x-q[1])/den, q[0]*(y-q[2])/den
 
+#electric potential
+def pot(q,x,y):
+    den = np.hypot(x-q[1],y-q[2])
+    return q[0]/den
+
 #color vectors
 def color(q,x,y):
     den = np.hypot(x-q[1],y-q[2])**1.01
@@ -30,19 +35,22 @@ def color(q,x,y):
 Ex = Ey = xcolor = ycolor = np.zeros((n,n))
 
 def onclick(event):
-
+    #value of |E| and V (arbitrary units)
     x = event.xdata
     y = event.ydata
 
-    Ex = Ey = 0
+    Ex = Ey = V = 0
 
     for q in qs:
-        ex, ey = e(q, x, y)
+        ex, ey = e(q,x,y)
+        v = pot(q,x,y)
         Ex += ex
         Ey += ey
+        V += v
 
     E = np.hypot(Ex,Ey)
     print('|E(%.2f,%.2f)| = %.2f'%(x,y,E))
+    print('V(%.2f,%.2f) = %.2f\n'%(x,y,V))
 
 for q in qs:
     ex, ey = e(q, x, y)
@@ -70,6 +78,8 @@ for q in qs:
 
 cid = fig1.canvas.mpl_connect('button_press_event', onclick)
 
+'''
+
 #field lines plot
 fig2 = plt.figure(2,(5,5))
 ax2 = plt.subplot(111)
@@ -87,7 +97,11 @@ plt.streamplot(x, y, Ex, Ey, color=color, linewidth=0.5, density=1.5, arrowstyle
 
 cid = fig2.canvas.mpl_connect('button_press_event', onclick)
 
+'''
+
 plt.show()
 
 plt.rcParams.update(plt.rcParamsDefault)
+
+
 
